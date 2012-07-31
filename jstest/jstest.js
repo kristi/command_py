2ca043de9e1544aae0465f307d7ab340b06740db
Kristi Tsukida <kristi.dev@gmail.com>
2012-07-30 18:01:52 -0700

    javascript implementation of command-t style sorting algorithm

---
+++ jstest/jstest.js
@@ -0,0 +1,174 @@
+function tscore(str, abbrev) {
+    var score = 1.0;
+    var m = {};
+    m.str = str;
+    filename = str.replace(/[^ ]*?\b(\w*)(?:\(\)|)(?: [({].*[)}]|)$/, '$1');
+    m.abbrev = abbrev;
+    m.max_score_per_char = (0.2 / (1 + filename.length) + 0.4 / (1 + str.length) + 1.2 / (1 + abbrev.length)) / 2;
+    if ( abbrev.length == 0 ) {
+        // nothing
+    } else {
+        score = recursive_match(m, 0, 0, 0, 0.0);
+        //m.str = filename;
+        //filescore = recursive_match(m, 0, 0, 0, 0.0);
+        //score += filescore * 0.2;
+    }
+    return score;
+}
+
+function recursive_match(m, str_idx, abbrev_idx, last_idx, score) {
+    var seen_score = 0;
+    for (var i = abbrev_idx; i < m.abbrev.length; i++) {
+        var c = m.abbrev[i];
+        var found = 0;
+        for (var j = str_idx; j < m.str.length; j++, str_idx++) {
+            var D = m.str[j];
+            var d = D.toLowerCase();
+            if ( c == d ) {
+                found = 1;
+                var score_for_char = m.max_score_per_char;
+                var distance = j - last_idx;
+                if ( distance > 1 ) {
+                    var factor = 1.0;
+                    var last = m.str[j - 1];
+                    var curr = m.str[j];
+                    if (last == "/") {
+                        factor = 0.8;
+                    } else if ( last == '.' ) {
+                        factor = 1.0;
+                    } else if ( last == "_" || last == " " || last == "-" || last == "(" || last == "{" || (last >= "0" && last <= "9") ) {
+                        factor = 0.6;
+                    } else {
+                        if ( last >= "a" && last <= "z" && curr >= "A" && curr <= "Z" ) {
+                            factor = 0.7;
+                        } else {
+                            factor = 0.3;
+                        }
+                        factor *= (1.0 / (1 + distance)) * 0.2;
+                    }
+                    score_for_char *= factor;
+                }
+                if (++j < m.str.length) {
+                    var sub_score = recursive_match(m, j, i, last_idx, score);
+                    if (sub_score > seen_score) {
+                        seen_score = sub_score;
+                    }
+                }
+
+                score += score_for_char;
+                last_idx = str_idx++;
+                break;
+            }
+            if ( d == "." ) {
+                //last_idx = str_idx++;
+                //last_idx = j;
+            }
+        }
+        if (found == 0) {
+            return 0.0;
+        }
+    }
+    return (score > seen_score) ? score : seen_score;
+}
+
+function regexEscape(str) {
+    return str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+}
+
+function kscore(str, abbrevRegex) {
+    var match = abbrevRegex.exec(str);
+    return match;
+}
+
+function submitQuery(e) {
+    e.preventDefault();
+    //var $form = $(this);
+    //var $input = $form.find("input");
+    var form = this;
+    var input = form.getElementsByTagName("input")[0];
+    var abbrev = input.value;
+    var abbrevArray = abbrev.split('');
+    for (var i = 0; i < abbrevArray.length; ++i) {
+        abbrevArray[i] = regexEscape(abbrevArray[i]);
+    }
+    var abbrevRegexStr = "(^.*)(?:" + abbrevArray.join(")(.*?)(?:") + ")";
+    var abbrevRegex = new RegExp(abbrevRegexStr, "i");
+    //console.log(kscore("hello", abbrevRegex));
+    //console.log(kscore("goodbye", abbrevRegex));
+
+    var links = document.getElementById("links");
+    var hiddenLinks = document.getElementById("hiddenLinks");
+    var linksParent = links.parentNode;
+    linksParent.removeChild(links);
+    linksParent.removeChild(hiddenLinks);
+    //var itemsArr = Array.prototype.slice.call(links.children);
+    var showArr = [];
+    var hideArr = [];
+    var unhideArr = [];
+    var itemsArr = links.children;
+    for (var i = 0; i < itemsArr.length; ++i) {
+        var item = itemsArr[i];
+        item.score = tscore(item.text, abbrev);
+        if (item.score < 0.2) {
+            hideArr.push(item);
+        } else {
+            showArr.push(item);
+        }
+    }
+    itemsArr = hiddenLinks.children;
+    for (var i = 0; i < itemsArr.length; ++i) {
+        var item = itemsArr[i];
+        item.score = tscore(item.text, abbrev);
+        if (item.score < 0.2) {
+            // already hidden, do nothing
+        } else {
+            unhideArr.push(item);
+            showArr.push(item);
+        }
+    }
+    showArr.sort(function(a, b) {
+        return b.score - a.score;
+    });
+    for (var i = 0; i < unhideArr.length; ++i) {
+        hiddenLinks.removeChild(unhideArr[i]);
+    }
+    for (var i = 0; i < hideArr.length; ++i) {
+        links.removeChild(hideArr[i]);
+        hiddenLinks.appendChild(hideArr[i]);
+    }
+    for (var i = 0; i < showArr.length; ++i) {
+        links.appendChild(showArr[i]);
+    }
+    linksParent.appendChild(links);
+    linksParent.appendChild(hiddenLinks);
+    console.log("done");
+
+    /*
+    $("#links a").each(function(index, el) {
+        var $this = $(this);
+        var str = $this.text();
+        var score = tscore(str, abbrev);
+        $this.score = score;
+        if (score == 0) {
+            $this.css('display', 'none');
+        } else {
+            $this.css('display', 'inline-block');
+        }
+    });
+    */
+
+    /*
+    var $links = $("#links a");
+    var display = [];
+    $links.detach().sort(function(a, b) {
+        return tscore($(b).text(), abbrev) - tscore($(a).text(), abbrev);
+    }).appendTo("#links");
+    */
+
+    return false;
+}
+$(function() {
+    console.log("hello");
+    //$("#search form").submit(submitQuery);
+    document.getElementById("search").getElementsByTagName("form")[0].onsubmit = submitQuery;
+});
